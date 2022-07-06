2c84bfbed0d8ac6bb23aed1d586496753c488ae2
souravkkr1 <kmrsourav786@gmail.com>
2022-07-06 20:45:32 +0530

    added first project

---
+++ Projects/calculator/index.js
@@ -0,0 +1,22 @@
+
+let key = document.querySelectorAll("#keys>div");
+for(let i=0; i<key.length; i++){
+    key[i].addEventListener("click", myCal);   
+}
+let bag="";
+
+function myCal(event){
+    let target=event.target.innerText;
+   
+
+    if(target=="C"){
+        document.querySelector("#display").innerText="";
+        bag="";
+    }else if(target=="="){
+        let res=eval(bag);
+        document.querySelector("#display").innerText=res;
+    }else{
+        bag=bag+target;
+        document.querySelector("#display").innerText=bag;
+    }
+}
