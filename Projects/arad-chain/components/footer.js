fed9fa4dc4f4f59b0f7e7bfbfbd3287ecc3f6d9f
Sourav Kumar <kmrsourav786@gmail.com>
2023-04-21 11:51:50 +0530

    add footer and navbar component

---
+++ Projects/arad-chain/components/footer.js
@@ -0,0 +1,62 @@
+function footer() {
+    return (
+        `
+        <div class="footer-container">
+        <div class="contact">
+          <h3>Quick Contact</h3>
+          <div>
+            <p>If you have any questions or need help, feel free to contact with our team.</p>
+            <a href="tel:(002)0000000000" class="text-accent text-hover-white">        <span class="cms-icon-wrap"><i aria-hidden="true" class="cms-icon cms-phone-icon text-16 cmsi cmsi-phone-alt" style=""></i></span>
+              &nbsp;(002) 0000000000</a>
+            <p>Dubai International Finance</p>
+            <a href="#">Get Directions</a>
+          </div>
+        </div>
+
+        <div class="company">
+          <h3>Company</h3>
+          <div>
+            <a href="#">Home</a>
+            <a href="#">Mission</a>
+            <a href="#">Registration</a>
+            <a href="#">Blogs</a>
+            <a href="#">About Us</a>
+          </div>
+        </div>
+
+        <div class="phases">
+          <h3>Phases</h3>
+          <div>
+            <a href="#">Phase 1</a>
+            <a href="#">Phase 2</a>
+            <a href="#">Phase 3</a>
+            <a href="#">Phase 4</a>
+            <a href="#">Phase 5</a>
+          </div>
+        </div>
+
+        <div class="benefits">
+          <h3>Benefits to</h3>
+          <div>
+            <p>Farmers</p>
+            <p>Manufacturers</p>
+            <p>Funders</p>
+            <p>Investors</p>
+            <p>Merchants & Traders</p>
+          </div>
+        </div>
+
+        <div class="contact-btn">
+          <button>Contact</button>
+          <div class="social-icons">
+
+          </div>
+        </div>
+
+
+      </div>
+        `
+    )
+}
+
+export default footer;
